a9ecdfd676575a39060423fbe0b03416a8d9e58f
Salutah <salutahmoha@gmail.com>
2024-10-21 15:22:28 +0300

    Building the Skills.jsx file to showcase my skills

--- src/Components/Pages/Skills.jsx
+++ src/Components/Pages/Skills.jsx
@@ -1,22 +1,22 @@
-// import React from "react";
-// import SkillStled from "../Skills/Skills-section";
-// import SkillsSectionData from "../Skills/SkillsData";
+import React from "react";
+import SkillStled from "../Skills/Skills-section";
+import SkillsSectionData from "../Skills/SkillsData";
 
-// function SkillSection() {
-//   return (
-//     <SkillStled>
-//       <div className="skills-container">
-//         <h3>Skills</h3>
-//         <div className="skills">
-//           {SkillsSectionData.map((Skills, index) => (
-//             <div className="skill decagon" key={index}>
-//               <img src={Skills.image} alt="" />
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </SkillStled>
-//   );
-// }
+function SkillSection() {
+  return (
+    <SkillStled>
+      <div className="skills-container">
+        <h3>Skills</h3>
+        <div className="skills">
+          {SkillsSectionData.map((Skills, index) => (
+            <div className="skill decagon" key={index}>
+              <img src={Skills.image} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </SkillStled>
+  );
+}
 
-// export default SkillSection;
+export default SkillSection;
